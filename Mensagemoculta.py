c0e28406436df88f9149db43c161cea699cb6402
victorlsilva694 <victorlsilva@outlook.com.br>
2021-04-05 19:02:24 -0300

    5 -Exercicio

--- Mensagemoculta.py
+++ Mensagemoculta.py
@@ -1,3 +1,28 @@
+
+"""
+    ****************************************************************************************************************
+    * Textos podem conter mensagens ocultas. Neste problema a mensagem oculta em um texto é composto               * 
+    * pelas primeiras letras de cada palavra do texto, na ordem em que aparecem.                                   *  
+    *                                                                                                              *  
+    * É dado um texto composto apenas por letras minúsculas ou espaços. Pode haver mais de um espaço               *  
+    * entre as palavras. O texto pode iniciar ou terminar em espaços, ou mesmo conter somente espaços.             *     
+    *                                                                                                              * 
+    * Entrada:                                                                                                     *  
+    *                                                                                                              *         
+    * A entrada contém vários casos de testes. A primeira linha de entrada contém um inteiro N que indica a        * 
+    * quantidade de casos de teste que vem a seguir. Cada caso de teste consiste de uma única linha contendo       *  
+    * de um a 50 caracteres, formado por letras minúsculas (‘a’-‘z’) ou espaços (‘ ’). Atenção para possíveis      * 
+    * espaços no início ou no final do texto!                                                                      * 
+    *                                                                                                              * 
+    * Saída                                                                                                        *     
+    *                                                                                                              *
+    * Para cada caso de teste imprima a mensagem oculta no texto de entrada.                                       * 
+    ****************************************************************************************************************
+
+
+
+"""
+
 Tests = int(input("Quantidade de testes: "))
 i = 0
 k = 0
